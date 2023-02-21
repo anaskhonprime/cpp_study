df3d0bdaf1110d1fb401072d4eed61c25397f056
anaskhonprime <smlife202@gmail.com>
2023-02-21 12:00:01 +0900

    Initial Chapter 1

---
+++ chapter1/chp1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+
+    cout <<"Running"<<endl;
+    return 0;
+}
---
+++ chapter1/fizzbuzz.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int n = 100;
+    
+    for(int i = 1; i <= n; i++){
+        
+        if( i % 3 == 0 && i % 5 == 0) {
+         cout << "FizzBuzz ";
+        }
+        else if( i % 3 == 0) {
+         cout << "Fizz ";
+        }
+        else if(i % 5 == 0) {
+         cout << "Buzz ";
+        }
+        else
+        cout << i << " ";
+    }
+    
+    return 0;
+}
